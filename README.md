d76e63738306a5eee500c584058137de4d50f579
Anshika Chourasia <79527285+anshi8269@users.noreply.github.com>
2023-10-22 15:07:39 +0530

    files added

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# InstaVista
- 📷 A Peek Inside Instagram: My latest project showcases a login page with a twist. It gives you a sneak peek into Instagram's world, alongside an intriguing escaping cursor that uncovers hidden words.  
+# InstaVistaA
---
+++ script.js
@@ -0,0 +1,304 @@
+var arr=[
+    {
+        "name" : "Shipli",
+        "song" : "New York hits",
+        "dp" : "https://images.unsplash.com/photo-1532171875345-9712d9d4f65a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=715&q=80",
+        "story" : "https://images.unsplash.com/photo-1532171875345-9712d9d4f65a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=715&q=80"
+    },
+    {
+        "name" : "Ansh Bbah",
+        "song" : "Senorita",
+        "dp" : "https://images.unsplash.com/photo-1584536682683-b79a8744eaa0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80",
+        "story" : "https://images.unsplash.com/photo-1583391265517-35bbdad01209?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
+    },
+    {
+        "name" : "Deep Singh",
+        "song" : "Love me thoda",
+        "dp" : "https://images.unsplash.com/photo-1579493934830-eab45746b51b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80",
+        "story" : "https://images.unsplash.com/photo-1581841064838-a470c740e8ee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
+    },
+    {
+        "name" : "Vaishh_Bro",
+        "song" : "Tu thodi der",
+        "dp" : "https://images.unsplash.com/photo-1508812831504-1dd7fdaca93d?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=687",
+        "story" : "https://images.unsplash.com/photo-1604514649606-2fdd0399de88?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=927&q=80"
+    },
+    {
+        "name" : "Exquisite_me",
+        "song" : "Shayrana",
+        "dp" : "https://images.unsplash.com/photo-1513210191510-69220f40601d?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=870",
+        "story" : "https://images.unsplash.com/photo-1604436553922-b8e12ec7224d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
+    }
+ 
+
+];
+
+var post=[
+    {
+        "name" : "Vaishh_dude",
+        "song" : "Tu thodi der",
+        "story" : "https://images.unsplash.com/photo-1604681527661-41ab3a47b2c4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=704&q=80",
+        "dp" : "https://images.unsplash.com/photo-1502323777036-f29e3972d82f?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=870"
+    },
+    {
+        "name" : "sim_me",
+        "song" : "Shayrana",
+        "story" : "https://images.unsplash.com/photo-1514654306380-538bac0ed82c?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=687",
+        "dp" : "https://images.unsplash.com/photo-1500336624523-d727130c3328?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=774"
+    },
+    
+    {
+        "name" : "Sidhharth_verma",
+        "song" : "Shayrana",
+        "story" : "https://images.unsplash.com/photo-1618453292507-4959ece6429e?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=764",
+        "dp" : "https://images.unsplash.com/photo-1500336624523-d727130c3328?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=774"
+    },
+    
+    {
+        "name" : "Rosi_me",
+        "song" : "Senorita",
+        "story" : "https://images.unsplash.com/photo-1462804993656-fac4ff489837?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=870",
+        "dp" : "https://images.unsplash.com/photo-1604674170775-4121de69ef01?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=927"
+    },
+    {
+        "name" : "Hey_its",
+        "song" : "Tu thodi der",
+        "story" : "https://images.unsplash.com/photo-1531891437562-4301cf35b7e4?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=764",
+        "dp" : "https://images.unsplash.com/photo-1502323777036-f29e3972d82f?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=870"
+    },
+    {
+        "name" : "Shipli",
+        "song" : "New York hits",
+        "story" : "https://images.unsplash.com/photo-1563992891888-3a441b92e7c7?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=687",
+        "dp" : "https://images.unsplash.com/photo-1604436154692-5f99b849232c?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=764"
+    },
+    {
+        "name" : "Deepi Singh",
+        "song" : "Love me thoda",
+        "story" : "https://images.unsplash.com/photo-1506795660198-e95c77602129?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=687",
+        "dp" : "https://images.unsplash.com/photo-1558694196-a711f3d291d7?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=774"
+    },
+    
+   
+ 
+
+];
+
+var clutter="";
+arr.forEach(function(elem , idx){
+    clutter+=`<div class="story">
+    <img id="${idx}" src="${elem.dp}" alt="">
+ </div>`
+});
+
+var storyi = document.querySelector(".storyi")
+storyi.innerHTML=clutter;
+
+storyi.addEventListener("click" , function(dets){
+    var fullscreen = document.querySelector("#full-screen")
+    var container = document.querySelector(".contain")
+    
+    fullscreen.style.backgroundImage=`url(${arr[dets.target.id].story})`
+    fullscreen.style.display="block"
+    container.style.display="none"
+
+    setTimeout(function(){
+        container.style.display="block"
+        fullscreen.style.display="none"
+        fullscreen.style.tranform="scale(.9) "
+       
+    },2000);
+
+
+    
+});
+
+// stories javascript ends
+
+// posts javascript
+
+//like button starts
+var insidecard = document.querySelector(".insidecard")
+var heart = document.querySelector("#heart")
+var like = document.querySelector("#like")
+
+insidecard.addEventListener("dblclick" , function(){
+
+    heart.style.transform="scale(1)"
+    heart.style.opacity="9"
+    heart.style.transition=' transform ease 2s'
+    like.style.color="pink"
+    setTimeout(function() {
+        heart.style.opacity="0"
+        heart.style.transform="scale(0)"
+    }, 2000);
+});
+
+// like button ends
+
+
+
+// post library starts
+
+var slutter="";
+post.forEach(function(elem ){
+    slutter+=` <div class="post">
+                       
+    <div class="posthead">
+        <img id="postdp" src="${elem.dp}" alt="">
+        <div>
+         <div id="posttext">${elem.name}</div>
+         <div id="songname">${elem.song}</div>
+        </div>
+        <span id="shareicon" class="material-symbols-outlined"> 
+         more_vert
+         </span>      
+     </div>
+
+<div class="postcontainer">
+    <img id="profile" src="${elem.story}" alt="" >
+</div>
+
+ 
+
+ <div class="posttail">
+     <span id="like" class="material-symbols-outlined">
+         favorite
+      </span>
+      <i id="comment" class="fa-regular fa-comment"></i>
+      <span id="share" class="material-symbols-outlined">
+         send
+     </span>
+
+     <span id="bookmark"
+     class="material-symbols-outlined">
+         bookmark
+         </span>
+         
+ </div>
+
+   </div>`
+});
+
+var container = document.querySelector(".post")
+container.innerHTML=slutter;
+
+/* cursor code */
+
+var main = document.querySelector("body")
+var cursor = document.querySelector(".cursor")
+
+
+main.addEventListener("mousemove" , function(e) {
+    cursor.style.left=e.x+"px";
+    cursor.style.top=e.y+"px";
+    
+});
+
+
+
+
+/*ends here */
+
+/*second container script*/
+
+var addfriend = document.querySelector("button")
+var body = document.querySelector("body")
+var card = document.querySelector(".second2container")
+var congrats = document.querySelector(".congrats")
+var bio = document.querySelector(".bio")
+
+var flag=true;
+addfriend.addEventListener("click" , function()
+{
+    if(flag==true)
+    {
+        addfriend.innerHTML = "Remove";
+        addfriend.style.backgroundColor="red"
+        card.style.backgroundImage="url(https://i.pinimg.com/originals/12/4d/e3/124de3d1b5e12f1d8fcec1685e634361.gif)"
+        congrats.innerText="Congratulation!! Now You have a Friend"
+        bio.style.opacity="0"
+        flag=false;
+    }
+    else{
+        addfriend.innerHTML = "Add Friend";
+        addfriend.style.backgroundColor="black"
+        card.style.backgroundColor=" rgba(235, 65, 226, 0.589)"
+        card.style.backgroundImage="none"
+        congrats.innerText=""
+        bio.style.opacity=""
+        flag=true;
+    }
+
+});
+
+/*ends here */
+
+/* reels script starts */
+
+var videos=[
+    {
+        "dp" : "https://images.unsplash.com/photo-1532171875345-9712d9d4f65a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=715&q=80",
+        "reel" : "https://player.vimeo.com/external/436028718.sd.mp4?s=c8fba8fa1270df64c68ade28270cac06aec0c06d&profile_id=165&oauth2_token_id=57447761"
+    },
+    {
+       
+        "dp" : "https://images.unsplash.com/photo-1584536682683-b79a8744eaa0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80",
+        "reel" : "https://player.vimeo.com/external/436525564.sd.mp4?s=8045153f616800c745bacbdd0b49d866e9d1ed25&profile_id=165&oauth2_token_id=57447761"
+    },
+    {
+        
+        "dp" : "https://images.unsplash.com/photo-1579493934830-eab45746b51b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80",
+        "reel" : "https://player.vimeo.com/external/380083890.sd.mp4?s=97b2ec93d457a30d8d34bf5f5b2595e921cf712a&profile_id=165&oauth2_token_id=57447761"
+    },
+    {
+        
+        "dp" : "https://images.unsplash.com/photo-1508812831504-1dd7fdaca93d?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=687",
+        "reel" : "https://player.vimeo.com/external/432156449.sd.mp4?s=9426204ffedaca7367f921828445788ea6f5e650&profile_id=165&oauth2_token_id=57447761"
+    },
+    {
+        
+        "dp" : "https://images.unsplash.com/photo-1513210191510-69220f40601d?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=870",
+        "reel" : "https://player.vimeo.com/external/559734827.sd.mp4?s=c513659871214b68fa30ae80caeb5678615a1eae&profile_id=165&oauth2_token_id=57447761"
+    }
+ 
+
+];
+
+var videocluter = ""
+
+videos.forEach(function(elem){
+    videocluter+=`<video src="${elem.reel}" autoplay muted></video>`;
+})
+
+var reels = document.getElementById("reels");
+var card = document.querySelector(".card");
+var insidecard = document.querySelector(".insidecard");
+var home = document.querySelector("#home");
+
+
+
+reels.addEventListener("click" , function(){
+   var fullscreen = document.querySelector(".contain-fullscreen")
+   var vid = document.querySelector("video")
+   card.style.display="none"
+   insidecard.style.display="none"
+   fullscreen.style.display="block"
+   fullscreen.innerHTML=videocluter;
+
+   
+});
+
+home.addEventListener("click", function(){
+    var fullscreen = document.querySelector(".contain-fullscreen")
+    card.style.display="block";
+    insidecard.style.display="block";
+    fullscreen.innerHTML="";
+    fullscreen.style.display="none";
+    
+})
+
+/*ends */
+
+
+
